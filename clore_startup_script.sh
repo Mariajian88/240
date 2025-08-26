8d6708951267fccb2b24635235e762dd93bead7c
mariajian <mariajian@gmail.com>
2025-08-26 06:52:03 -0700

    startup script

---
+++ clore_startup_script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Update and Install necessary packages
+apt update -y && apt install unzip git wget nano libzstd-dev libzstd1 vim python3 cuda-cudart-12-8 joe zip screen curl libcurl4 libcudart12 build-essential gcc make -y
+
+# # Clone the repository
+# git clone https://github.com/Mariajian88/240.git
+
+# create Screen
+screen -S vanity
+
+# # Navigate to the cloned repository
+# cd 240
+
+# Make the vanitysearch executable
+chmod +x /root/240/vanitysearch
+
+# Run the python script with arguments
+python3 /root/240/start_prefix_vanity.py "$@"
